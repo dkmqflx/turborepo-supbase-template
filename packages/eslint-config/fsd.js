4c07e0b2885c09bd2e0566862657f35f78ca5d14
dkmqflx <dkmqflx@gmail.com>
2025-04-14 14:48:59 +0900

    refactor: migrate to @feature-sliced/eslint-config for FSD rules

--- packages/eslint-config/fsd.js
+++ packages/eslint-config/fsd.js
@@ -1,31 +1,21 @@
 /* eslint-disable no-undef */
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['@repo/eslint-config/next.js'],
-  plugins: ['import'],
+  extends: ['@feature-sliced', '@repo/eslint-config/next.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
-  rules: {
-    'import/no-restricted-paths': [
-      'error',
-      {
-        zones: [
-          {
-            target: './src/features/**/*',
-            from: ['./src/app/**/*', './src/widgets/**/*'],
-          },
-          {
-            target: './src/entities/**/*',
-            from: ['./src/app/**/*', './src/widgets/**/*', './src/features/**/*'],
-          },
-          {
-            target: './src/shared/**/*',
-            from: ['./src/app/**/*', './src/widgets/**/*', './src/features/**/*', './src/entities/**/*'],
-          },
-        ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
       },
-    ],
+    },
+  },
+  rules: {
+    'import/order': 'off',
+    'import/no-internal-modules': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
   },
 };
