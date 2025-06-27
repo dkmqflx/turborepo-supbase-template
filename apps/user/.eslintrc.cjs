3da3487c14205c0f9fe378c653236085971272ef
dkmqflx <dkmqflx@gmail.com>
2025-06-27 15:58:59 +0900

    chore: add tansk query lint

--- apps/user/.eslintrc.cjs
+++ apps/user/.eslintrc.cjs
@@ -1,5 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@repo/eslint-config/fsd'],
+  extends: ['@repo/eslint-config/fsd', 'plugin:@tanstack/query/recommended'],
 };
