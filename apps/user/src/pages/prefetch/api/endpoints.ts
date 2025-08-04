25508dc20009bc4669efc7195369ead4ece8abf0
dkmqflx <dkmqflx@gmail.com>
2025-08-04 19:37:28 +0900

    fix: type error

--- apps/user/src/pages/prefetch/api/endpoints.ts
+++ apps/user/src/pages/prefetch/api/endpoints.ts
@@ -2,8 +2,8 @@ import { client } from '@/shared/lib/apiClient';
 
 import type { Post } from './types';
 
-export const getPosts = async (): Promise<Post[]> => {
-  const { data } = await client.get('/api/posts');
+export const getPosts = async () => {
+  const { data } = await client.get<Post[]>('/api/posts');
 
   return data;
 };
